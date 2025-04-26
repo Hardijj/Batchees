1c42f0b7ca8d64c12947c07d007207e323e75540
Hardijj <kalujiku3@gmail.com>
2025-04-26 14:28:32 +0530

    Update VideoPlayer.jsx

--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -188,7 +188,7 @@ const VideoPlayer = () => {
   };
 
   const handleGoToDownloadClick = () => {
-    const fileName = ${chaptersName} ${lecturesName}; // You can customize filename if you want
+    const fileName = ${chaptersName, lecturesName}; // You can customize filename if you want
     const downloadUrl= m3u8Url;
     const intentUrl = `intent:${downloadUrl}#Intent;action=android.intent.action.VIEW;package=idm.internet.download.manager;scheme=1dmdownload;S.title=${encodeURIComponent(fileName)};end`;
     
