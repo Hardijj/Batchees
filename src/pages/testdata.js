604abe43d377948f8d8c9707ca6a80c862d3a834
Hardijj <kalujiku3@gmail.com>
2025-05-31 12:14:27 +0530

    Update testdata.js

--- src/pages/testdata.js
+++ src/pages/testdata.js
@@ -2,17 +2,29 @@ const testData = {
   Maths: {
     "Test 1": [
       {
-        question: "What is 2 + 2?",
-        options: ["2", "4", "5", "22"],
+        question: "The exponent of 2 in the prime factorization of 144, is",
+        options: ["4", "5", "6", "3"],
         correctAnswer: 1,
-        explanation: "2 + 2 = 4."
+        explanation: "Prime Factorisation karne ke baad 2 jitni baar repeat ho rha hai wo"
       },
       {
-        question: "What is 10 / 2?",
-        options: ["2", "5", "10", "20"],
-        correctAnswer: 1,
-        explanation: "10 divided by 2 equals 5."
-      }
+        question: "If two positive integers a and b are expressible in the form a = pq² and b = p³q; p, q being prime numbers, then LCM (a, b) is:",
+        options: ["pq", "p³q³", "p³q²", "p²q²"],
+        correctAnswer: 3,
+        explanation: "LCM bro/sis .."
+      },
+      {
+        question: "If the sum of LCM and HCF of two numbers is 1260 and their LCM is 900 more than their HCF, then the product of two numbers is:",
+        options: ["203400", "194400", "198400", "205400"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "Two tankers contain 850 litres and 680 litres of petrol respectively. Find the maximum capacity of a container which can measure the petrol of either tanker in exact number of times:",
+        options: ["245", "190", "170", "125"],
+        correctAnswer: 3,
+        explanation: ""
+      },
     ],
   },
   Science: {
